6913902519117fb66f6833160dd05199618f177b
vijay25890 <vijaymore25890@gmail.com>
2021-09-07 13:29:03 +0530

    switch

--- doWhileLoop.cpp
+++ doWhileLoop.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
-    
     do
     {
         cout<<n;
     } while (n>0);
-    
     return 0;
 } 
--- whileLoop.cpp
+++ whileLoop.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-
     while (n>0)
     {
         cout<<n<<endl;
         cin>>n;
     }
-    
     return 0;
 } 
