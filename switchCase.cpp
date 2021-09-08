69290a15588f06fdc5431333606c27db12764819
vijay25890 <vijaymore25890@gmail.com>
2021-09-08 12:41:03 +0530

    operator

---
+++ switchCase.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char button;
+    cout<<"Enter a Char";
+    cin>>button;
+    switch(button)
+    {
+        case 'a':
+        cout<<"a is pressed";
+        break;
+    
+        case 'b':
+        cout<<"b is pressed";
+        break;
+
+        default:
+        cout<<"enter correct char";
+        break;
+    }
+}
