6a3c76db1b000259ab307d6ccb99346a5bf19cdc
vijay25890 <vijaymore25890@gmail.com>
2021-10-16 12:18:25 +0530

    inclusion

--- inclusion.cpp
+++ inclusion.cpp
@@ -1,9 +1,20 @@
-//no of ways to do something
-#include<iostream>
+// no of ways to do something 
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+int divisible(int n, int a, int b)
+{
+    int c1 = n / a;
+    int c2 = n / b;
+    int c3 = n / (a * b);
 
-    return 0;
+    return c1 + c2 - c3;
 }
+
+int main()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << divisible(n, a, b) << endl;
+    return 0; 
+} 
